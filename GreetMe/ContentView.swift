e4ac6e1c5f2a38910e8733f14cbbde792a90c602
The Najee Jeremiah Company <najeejere@gmail.com>
2026-06-25 22:40:08 -0400

    Initial Commit

---
+++ GreetMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreetMe
+//
+//  Created by Yakubu Naji Denoko on 6/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreetMe/GreetMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetMeApp.swift
+//  GreetMe
+//
+//  Created by Yakubu Naji Denoko on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct GreetMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
